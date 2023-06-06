cf7d74815e7ee607f72b178c2112cbc075f25165
WebAxol <webaxol@gmail.com>
2023-06-06 09:32:54 -0600

    The framework was packed into pluglight.min.js at /dist using webpack and terser-webpack-plugin.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import TerserPlugin from "terser-webpack-plugin";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+export default  {
+    entry : './src/World.js',
+    output: {
+        path : path.resolve(__dirname,'dist'),
+        filename : 'pluglight.min.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /\.js/,
+                loader  : 'babel-loader',
+                options : {
+                    presets : ['@babel/preset-env']
+                }
+            }
+        ]
+    },
+    optimization : {
+        minimize  : true,
+        minimizer : [new TerserPlugin()] 
+    }
+}
