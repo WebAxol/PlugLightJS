931b5410281e9fe468b9a8d6f9fdf45e570475cc
WebAxol <webaxol@gmail.com>
2023-12-23 07:11:51 -0600

    The 'Entity' class was separated to it's own file 'Entity.js' and is imported by 'EntityPool.js'

---
+++ src/Entity.js
@@ -0,0 +1,63 @@
+class Entity{
+
+    #type;
+    #collections;
+    #ID;
+
+    constructor(typeName,prototype, ID){
+
+        this._children = {};
+        this.#collections = {};
+        this.#type = typeName;
+        this.#ID   = ID;
+
+        if(!prototype['info']) return;
+
+        Object.keys(prototype['info']).forEach((field) => {
+            
+            let _field =  prototype['info'][field];
+
+            if(typeof _field == 'object') this[field] = Object.assign({},_field); 
+
+            else this[field] = _field;
+        });
+        
+    }
+
+    getID(){
+        return this.#ID;
+    }
+
+    getType(){
+        return this.#type;
+    }
+
+    isInCollection(collectionName){
+        return this.#collections[collectionName];
+    }
+
+    getCollections(){
+        return Object.keys(this.#collections);
+    }
+
+    addCollection(collectionName){
+        this.#collections[collectionName] = 1;
+    }
+    removeCollection(collectionName){
+        delete this.#collections[collectionName];
+    }
+    reset(prototype, newID){
+
+        this.#ID = newID;
+
+        Object.keys(prototype['info']).forEach((field) => {
+            let _field =  prototype['info'][field];
+            
+            if(typeof _field == 'object') this[field] = Object.assign({},_field); 
+            
+            else this[field] = _field;
+        });
+    }
+}
+
+export default Entity;
--- src/EntityPool.js
+++ src/EntityPool.js
@@ -1,3 +1,5 @@
+import Entity from "./Entity.js";
+
 class EntityPool{
 
     #types;
@@ -12,67 +14,7 @@ class EntityPool{
         this.toBeRemoved = [];
         this.#nextID = 0;
         
-        this.#Entity = class Entity{
-
-            #type;
-            #collections;
-            #ID;
-
-            constructor(typeName,prototype, ID){
-
-                this._children = {};
-                this.#collections = {};
-                this.#type = typeName;
-                this.#ID   = ID;
-
-                if(!prototype['info']) return;
-
-                Object.keys(prototype['info']).forEach((field) => {
-                    
-                    let _field =  prototype['info'][field];
-
-                    if(typeof _field == 'object') this[field] = Object.assign({},_field); 
-
-                    else this[field] = _field;
-                });
-                
-            }
-
-            getID(){
-                return this.#ID;
-            }
-
-            getType(){
-                return this.#type;
-            }
-
-            isInCollection(collectionName){
-                return this.#collections[collectionName];
-            }
-
-            getCollections(){
-                return Object.keys(this.#collections);
-            }
-
-            addCollection(collectionName){
-                this.#collections[collectionName] = 1;
-            }
-            removeCollection(collectionName){
-                delete this.#collections[collectionName];
-            }
-            reset(prototype, newID){
-
-                this.#ID = newID;
-
-                Object.keys(prototype['info']).forEach((field) => {
-                    let _field =  prototype['info'][field];
-                    
-                    if(typeof _field == 'object') this[field] = Object.assign({},_field); 
-                    
-                    else this[field] = _field;
-                });
-            }
-        }
+        this.#Entity = Entity;
     }
 
     registerType(typeName,prototype){
